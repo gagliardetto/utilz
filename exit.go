342041b198439cd378c2ffc39d3360d48a20db06
Slavomir <gagliardetto@users.noreply.github.com>
2020-08-15 16:34:46 +0200

    Add HardWaitSystemSignal; Add optional messages to WaitSystemSignal

--- exit.go
+++ exit.go
@@ -1,6 +1,7 @@
 package utilz
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
 	"sync"
@@ -24,10 +25,50 @@ func Notify(handler Handler, signals ...os.Signal) {
 func DefaultNotify(handler func(os.Signal) bool) {
 	Notify(handler, os.Interrupt, os.Kill)
 }
-func WaitSystemSignal() {
+
+func WaitSystemSignal(messages ...string) {
 	DefaultNotify(func(_ os.Signal) bool {
+		defer func() {
+			fmt.Fprintln(os.Stderr, toInterfaceArray(messages)...)
+		}()
 		return false
 	})
+}
+
+// HardWaitSystemSignal calls the callback at first signal, and waits for
+// the callback to finish; when the callback finishes, HardWaitSystemSignal also finishes.
+// If a second sygnal is sent before the callback finishes, a hard os.Exit is done.
+func HardWaitSystemSignal(callback func(), messages ...string) {
+	signalNum := 0
+
+	pipe := make(chan os.Signal, 1)
+	signal.Notify(pipe, os.Interrupt, os.Kill)
+
+Loop:
+	for {
+		select {
+		case _, ok := <-pipe:
+			if !ok {
+				return
+			}
+			if signalNum == 0 {
+				signalNum++
+
+				fmt.Fprintln(os.Stderr, toInterfaceArray(messages)...)
+
+				go func() {
+					defer close(pipe)
+					callback()
+				}()
+				continue Loop
+			} else {
+				Ln("Forcing exit")
+				os.Exit(1)
+			}
+
+		}
+	}
+
 }
 
 // WaitHasTimedout waits for the waitgroup for the specified max timeout.
