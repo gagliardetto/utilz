03220bba736ad7a761189ff9ed7c760fe1bafb7a
Slavomir <gagliardetto@users.noreply.github.com>
2020-12-02 17:55:04 +0100

    Rename Handler to OsSignalHandler

--- exit.go
+++ exit.go
@@ -8,11 +8,11 @@ import (
 	"time"
 )
 
-type Handler func(os.Signal) bool
+type OsSignalHandler func(os.Signal) bool
 
 // Notify calls handler when gets specified signals and pass given signal to
 // handler. If handler returns false, notify stops waiting for signals.
-func Notify(handler Handler, signals ...os.Signal) {
+func Notify(handler OsSignalHandler, signals ...os.Signal) {
 	pipe := make(chan os.Signal, 1)
 	signal.Notify(pipe, signals...)
 
