3a0a72a189089c1e952547e1da4370a00766a30c
Slavomir <gagliardetto@users.noreply.github.com>
2020-12-15 00:24:45 +0100

    Improve CombineErrors

--- debug.go
+++ debug.go
@@ -464,7 +464,18 @@ func (ce *CombinedErrors) Error() string {
 	}
 	return buf.String()
 }
+func allNil(errs ...error) bool {
+	for _, err := range errs {
+		if err != nil {
+			return false
+		}
+	}
+	return true
+}
 func CombineErrors(errs ...error) error {
+	if len(errs) == 0 || allNil(errs...) {
+		return nil
+	}
 	return &CombinedErrors{
 		errs: errs,
 	}
